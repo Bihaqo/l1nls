4e595a771b9eb0240bfa9e58b3d0f43f99821388
Alex Novikov <novikov@bayesgroup.ru>
2015-01-05 15:52:56 +0300

    Python version (slow)

---
+++ regularized_ls.py
@@ -0,0 +1,21 @@
+import pulp
+
+def regularized_ls(A, b):
+    """Solve system of linear equations regularized by the 1-norm.
+
+    Solves an optimization problem
+    min. ||x||_1
+    s.t.
+    A x = b
+    where ||x||_1 is the 1-norm of the vector x.
+    """
+    [m, n] = A.shape
+    prob = pulp.LpProblem("Linear System", pulp.LpMinimize)
+    pos_vars = pulp.LpVariable.dicts("Positive", range(n), 0)
+    neg_vars = pulp.LpVariable.dicts("Negative", range(n), 0)
+    for eq in range(m):
+        prob += pulp.lpSum((pos_vars[i] - neg_vars[i]) * A[eq][i] for i in range(n)) == b[eq]
+
+    val = prob.solve()
+    x = [pulp.value(pos_vars[i]) - pulp.value(neg_vars[i]) for i in range(n)]
+    return (x, val)
